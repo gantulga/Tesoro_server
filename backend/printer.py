66c887d57e0a788b60f65db172e8885802890d49
gantulga <gantulga_tdb@yahoo.com>
2023-02-08 15:05:49 +0800

    dsad

--- backend/printer.py
+++ backend/printer.py
@@ -1,11 +1,11 @@
 import socket              
 mysocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)         
 host = "192.168.1.9" 
-port = 9100   
+port = 9100
 try:           
 	mysocket.connect((host, port)) #connecting to host
 	print("trying")
-	mysocket.send(b"^XA^A0N,50,50^FO50,50^FDSocket Test^FS^XZ")#using bytes
+	mysocket.send(b"^XA^A0N,50,50^FO50,50^FDSocket Test^FS^XZ") #using bytes
 	mysocket.close () #closing connection
 except:
 	print("Error with the connection")
