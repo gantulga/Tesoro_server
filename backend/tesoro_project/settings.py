f4fe6a597c30c9629c2db350604f50b23a753239
gantulga.da <gantulga.da@erdenetmc.mn>
2023-02-14 10:08:39 +0800

    Dasdas

--- backend/tesoro_project/settings.py
+++ backend/tesoro_project/settings.py
@@ -103,24 +103,22 @@ WSGI_APPLICATION = 'tesoro_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     # 'NAME': 'tesoro_mn',
-    #     'NAME': 'tesoro',
-    #     'USER': 'root',
-    #     'PASSWORD': 'Fa90101830#',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'tesoro_mn',
         'NAME': 'tesoro',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Fa90101830#',
         'HOST': 'localhost',
         'PORT': '3306',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'tesoro',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 }
 
 
