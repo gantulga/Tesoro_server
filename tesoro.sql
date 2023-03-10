d9339ff93853127fc8831def83aaac956984f80d
gantulga <gantulga_tdb@yahoo.com>
2023-03-10 21:25:58 +0800

    dasdasd

--- tesoro.sql
+++ tesoro.sql
@@ -1948,7 +1948,7 @@ CREATE TABLE `structure_app_error_list` (
   `id` int NOT NULL AUTO_INCREMENT,
   `error_value` longtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=696 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=696 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
