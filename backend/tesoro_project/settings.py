96bc2d491b30d400eb21aef3d48007b6107c2cf4
gantulga <gantulga_tdb@yahoo.com>
2023-03-12 11:04:00 +0800

    adad

--- backend/tesoro_project/settings.py
+++ backend/tesoro_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^$ja78mdk!!43dl)7x1g5=@2wnmu018h1n#_(fr6qc&_*+s74h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
