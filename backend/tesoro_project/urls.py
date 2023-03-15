b3b638899755c8a792a4abe1238a6a8e5940bef2
gantulga <gantulga_tdb@yahoo.com>
2023-03-16 00:41:20 +0800

    asdsa

--- backend/tesoro_project/urls.py
+++ backend/tesoro_project/urls.py
@@ -154,11 +154,13 @@ urlpatterns = [
 
     #tur zuuriin
     path('itemBalanceChanger/<int:client_id>', itemBalanceChanger), #uil ajillagaa jigdreheer idevhgui bolgoh
-
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+        re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    ]
