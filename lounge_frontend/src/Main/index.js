754f645bac6397b8b18c8f67b3f8a9792d7c70d5
gantulga <gantulga_tdb@yahoo.com>
2025-09-02 21:35:36 +0800

    asdasda

--- lounge_frontend/src/Main/index.js
+++ lounge_frontend/src/Main/index.js
@@ -18,7 +18,7 @@ export default class index extends Component {
       token: null,
       user: null,
       isAuthenticated: "",
-      ip_address: "192.168.1.8:8000",
+      ip_address: "112.72.8.158:8000",
       nextPath: "default",
     };
   }
