5eac7e2508c22f3c86428ed446c04c733dd14b7b
gantulga <gantulga_tdb@yahoo.com>
2025-11-09 17:36:37 +0800

    Adsada

--- backend/product_app/models.py
+++ backend/product_app/models.py
@@ -75,6 +75,7 @@ class Commodity(Createdinfo):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
+        print(self.categories.all())
         for cat in self.categories.all():
             if cat.parent:
                 self.categories.add(cat.parent)
