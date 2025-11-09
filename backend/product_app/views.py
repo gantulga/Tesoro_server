6c5c77e83a01c431cf1555d060d356298668d85a
gantulga <gantulga_tdb@yahoo.com>
2025-11-09 13:56:04 +0800

    asdadas

--- backend/product_app/views.py
+++ backend/product_app/views.py
@@ -15,13 +15,10 @@ def balanceChecker(request, client_id, product_id):
         if len(product.ingredients.all()) == 0:
             balance = 1000
         else:
-            print("product.ingredients.all()", product.ingredients.all())
             for ingredient in product.ingredients.all():
                 size_type = ingredient.size_type.abbreviation
-                print("--------------", ingredient, size_type)
                 if size_type == 'гр':
                     balance_obj = Item_balance.objects.filter(client=client.id, commodity=ingredient.commodity.id)
-                    print("item balance", len(balance_obj))
                     if len(balance_obj) > 0:
                         if balance_obj[0].size >= ingredient.size:
                             b = math.floor(balance_obj[0].size / ingredient.size)
@@ -42,7 +39,6 @@ def balanceChecker(request, client_id, product_id):
                                 data = {'product_id' : p_product.id, 'product_name': p_product.name, 'quantity': p_product_balance[0].quantity}
                                 zadlah_bolomjtoi_products.append(data)
             
-            print("product.ingredients_producted.all()", product.ingredients_producted.all())
             for ingredient_product in product.ingredients_producted.all():
                 balance_obj2 = Item_balance.objects.filter(client=client.id, product=ingredient_product.commodity.id)
                 print("-------------- item balance", ingredient_product, len(balance_obj2))
